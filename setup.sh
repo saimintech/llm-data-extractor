8d73043e8841d4e3de3d9eea5a0adfa0f5e8346e
Saim Safdar <53660940+saimintech@users.noreply.github.com>
2024-09-30 17:40:27 +0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install Chrome
+sudo apt update
+sudo apt install -y wget gnupg
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt update
+sudo apt install -y google-chrome-stable
+
+# Install Python dependencies
+pip install -r requirements.txt
